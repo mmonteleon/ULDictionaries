31d1a7c65ab74913f9bd86daa5255c24bbda58fa
EC2 Default User <ec2-user@ip-172-31-34-164.us-east-2.compute.internal>
2019-07-10 12:47:54 +0000

    first commit

---
+++ pika2.py
@@ -0,0 +1,16 @@
+# Pikachu's name, Pokedex, height, weight, type
+# Source: https://www.pokemon.com/us/pokedex/pikachu
+pika = {
+    "name": "Pikachu",
+    "pokedex index": 25,
+    "height": "16 inches",
+    "weight": "13.2 lbs",
+    "type": "electric"
+}
+
+print(pika)
+
+print(pika["type"])
+
+for item in pika:
+    print(item)
